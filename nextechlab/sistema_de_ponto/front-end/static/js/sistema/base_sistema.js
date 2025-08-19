c9712b474ceebb7091ef6bb24ba69a716db3aea1
mira <gllimamira@gmail.com>
2025-08-19 19:00:48 -0300

    funcionalidades e front feito

--- nextechlab/sistema_de_ponto/front-end/static/js/sistema/base_sistema.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const cadastroLink = document.getElementById('cadastro-link');
-    cadastroLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        alert('Você já está cadastrado!');
-    });
-});
